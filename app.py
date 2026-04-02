fc363f2a37c6d530fcc379d62b166502774c55f2
a49761020-cyber <a49761020@gmail.com>
2026-04-02 23:41:55 +0900

    fix port 8080

--- app.py
+++ app.py
@@ -406,4 +406,4 @@ if __name__ == '__main__':
     print('  브라우저에서 http://localhost:5000 접속하세요')
     print('=' * 50)
     import os
-app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
+app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT',8080)))
